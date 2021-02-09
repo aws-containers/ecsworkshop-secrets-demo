d977d313d051642a203537b6aa92ad4e5eaebcca
Tuszynski <tuszym@147dda3cd1b5.ant.amazon.com>
2021-02-09 09:51:50 -0800

    first try env vars from container

--- src/dbconfig/dbconnector.ts
+++ src/dbconfig/dbconnector.ts
@@ -1,7 +1,9 @@
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 export default new Pool ({
     max: 20,
-    connectionString: 'postgres://postgres:newPassword@postgres:5432/tododb',
+    connectionString: `postgres://postgres:${process.env.SECRET_NAME}@${process.env.DATABASE_ENDPOINT}:5432/tododb`,
     idleTimeoutMillis: 30000
 })

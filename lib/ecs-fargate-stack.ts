7a38a31d1a7a32771a7f690babd3cf0db57d285a
Henrique Santana <cssntan@amazon.com>
2022-11-10 14:01:18 +0000

    Upgrading to CDK version 2.0

--- lib/ecs-fargate-stack.ts
+++ lib/ecs-fargate-stack.ts
@@ -1,7 +1,7 @@
 import { App, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ecs from 'aws-cdk-lib';
-import * as ecsPatterns from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as sm from 'aws-cdk-lib/aws-secretsmanager';
 
 export interface ECSStackProps extends StackProps {
@@ -18,8 +18,9 @@ export class ECSStack extends Stack {
     const containerImage = this.node.tryGetContext("containerImage");
     const creds = sm.Secret.fromSecretCompleteArn(this, 'postgresCreds', props.dbSecretArn);
 
-    const cluster = new ecs.Cluster(this, 'fargateClusterDemo', {
+    const cluster = new ecs.Cluster(this, 'Cluster', {
       vpc: props.vpc,
+      clusterName: 'fargateClusterDemo'
     });
 
     const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, "fargateService", {
